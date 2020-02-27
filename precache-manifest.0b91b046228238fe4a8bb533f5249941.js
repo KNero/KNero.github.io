cb4f37156b104c4bd6871b13a0668239888517f0
KwonSeongmin <kwonsm@icloud.com>
2020-02-27 23:49:12 +0900

    modify

---
+++ precache-manifest.0b91b046228238fe4a8bb533f5249941.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ad3bf213c2a9970676eff8d4c9bd4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c60ca7e6f661ab2c6b51",
+    "url": "/static/css/main.9bad9c2c.chunk.css"
+  },
+  {
+    "revision": "0a0cf7c46792e9217f0f",
+    "url": "/static/js/2.b2d9299c.chunk.js"
+  },
+  {
+    "revision": "f96c07d9f455e7cc5e03d5121c1c7a4e",
+    "url": "/static/js/2.b2d9299c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c60ca7e6f661ab2c6b51",
+    "url": "/static/js/main.1d60673a.chunk.js"
+  },
+  {
+    "revision": "4ce6854054095d8732f7",
+    "url": "/static/js/runtime-main.8d1dad40.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5dfe6a67be684f61b7cf98e1d3e44f92.js"
+  "/precache-manifest.0b91b046228238fe4a8bb533f5249941.js"
 );
 
 self.addEventListener('message', (event) => {
